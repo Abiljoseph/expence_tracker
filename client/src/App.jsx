bc6261fa49a33e1415b0795313d4eb290589304d
rohithr99 <rohithrvarma99@gmail.com>
2023-11-03 15:15:16 +0530

    added add.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import SignUp from "./components/Auth/SignUp";
+import AddExpense from "./components/pages/AddExpense";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/add_expense" element={<AddExpense></AddExpense>}></Route>
       </Routes>
     </BrowserRouter>
   );
